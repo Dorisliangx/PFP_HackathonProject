9ad7bb2c9b2a973b148053a5a3cd2245b900b162
Doris <92877001+Dorisliangx@users.noreply.github.com>
2024-04-11 22:52:35 +0800

    Add files via upload

---
+++ PFP_Project/utils/util.js
@@ -0,0 +1,49 @@
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+function numberComma(source, length = 3) {
+  source = String(source).split(".")
+  source[0] = source[0].replace(new RegExp('(\\d)(?=(\\d{' + length + '})+$)', 'ig'), "$1,")
+  return source.join(".")
+}
+
+/**
+* 分享配置
+* @param  {String} title  [Title]
+* @param  {String} path   [Path]
+* @return {Object}        [Share Config]
+*/
+const shareConfig = (option = {}) => {
+  let title = '你可能看到的是假收益'
+  let path = '/pages/index/index'
+
+  if (option.title && option.title != '') title = option.title
+
+  if (option.path && option.path != '') path = option.path
+
+  return {
+      title: title,
+      path: path
+  }
+}
+
+module.exports = {
+  formatTime,
+  numberComma,
+  shareConfig
+}
